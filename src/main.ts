89b4c7209dc17298b0fe57739835493547c36afd
Germey <cqc@cuiqingcai.com>
2021-03-19 20:12:31 +0800

    add element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './registerServiceWorker';
 
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -10,4 +11,4 @@ import './styles/index.scss';
 import { Loader } from '@/directives/Loader';
 import { Logger } from '@/plugins/Logger';
 
-createApp(App).use(store).use(router).directive('loader', Loader).use(Logger).mount('#app');
+createApp(App).use(store).use(router).use(ElementPlus).directive('loader', Loader).use(Logger).mount('#app');
