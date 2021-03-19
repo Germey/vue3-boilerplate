7392ad407d3e8f3b4faaf42f5292f6ed9824b9bc
Germey <cqc@cuiqingcai.com>
2021-03-19 20:37:54 +0800

    add axios

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import ElementPlus from 'element-plus';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import './styles/index.scss';
 
 import { Loader } from '@/directives/Loader';
 import { Logger } from '@/plugins/Logger';
 
-createApp(App).use(store).use(router).use(ElementPlus).directive('loader', Loader).use(Logger).mount('#app');
+createApp(App).use(store).use(router).use(VueAxios, axios).use(ElementPlus).directive('loader', Loader).use(Logger).mount('#app');
