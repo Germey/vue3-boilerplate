c8f2694363dc904c068fb3bc102c756bd0dee5ae
崔庆才丨静觅 <cqc@cuiqingcai.com>
2021-03-20 17:28:24 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import store from './store';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import './styles/index.scss';
+import 'element-plus/lib/theme-chalk/index.css';
 
 import { Loader } from '@/directives/Loader';
 import { Logger } from '@/plugins/Logger';
